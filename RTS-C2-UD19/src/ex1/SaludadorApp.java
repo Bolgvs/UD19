0374b00a43a1c04b6d08f67e9d4a71f1369636f8
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-29 10:06:13 +0200

    Commit ejercicio1

--- RTS-C2-UD19/src/ex1/SaludadorApp.java
+++ RTS-C2-UD19/src/ex1/SaludadorApp.java
@@ -3,8 +3,7 @@ package ex1;
 public class SaludadorApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		SaludadorWindow Saludador = new SaludadorWindow();
 	}
 
 }
--- RTS-C2-UD19/src/ex1/SaludadorWindow.java
+++ RTS-C2-UD19/src/ex1/SaludadorWindow.java
@@ -6,6 +6,8 @@ import java.awt.Rectangle;
 import javax.swing.JFrame;
 import java.awt.Window.Type;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JTextField;
@@ -14,30 +16,18 @@ import java.awt.event.ActionEvent;
 
 public class SaludadorWindow {
 
+	private Saludador salutatous = new Saludador();
 	private JFrame Saludador;
 	private JTextField textField;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					SaludadorWindow window = new SaludadorWindow();
-					window.Saludador.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+
 
 	/**
 	 * Create the application.
 	 */
 	public SaludadorWindow() {
 		initialize();
+		Saludador.setVisible(true);
 	}
 
 	/**
@@ -54,17 +44,22 @@ public class SaludadorWindow {
 		label.setBounds(114, 12, 244, 54);
 		Saludador.getContentPane().add(label);
 		
+		JTextField texto = new JTextField();
+		texto.setBounds(83, 132, 293, 19);
+		Saludador.getContentPane().add(texto);
+		// textField.setColumns(10);
+		
 		JButton boton = new JButton("¡Saludar!");
 		boton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				if (!texto.getText().isEmpty()) {
+				JOptionPane.showMessageDialog(null, salutatous.saludar(texto.getText()));
+				} else {
+					JOptionPane.showMessageDialog(null, "Debes poner un nombre en el recuadro");
+				}
 			}
 		});
 		boton.setBounds(160, 199, 117, 25);
 		Saludador.getContentPane().add(boton);
-		
-		JTextField texto = new JTextField();
-		texto.setBounds(83, 132, 293, 19);
-		Saludador.getContentPane().add(texto);
-		textField.setColumns(10);
 	}
 }
