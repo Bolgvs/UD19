3d2d9e3cd20bbc8ad8eaf3afbc0808187c968a5e
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-07-29 09:43:49 +0200

    Task1: Add basic graphical interface for the applet

--- RTS-C2-UD19/src/ex1/SaludadorWindow.java
+++ RTS-C2-UD19/src/ex1/SaludadorWindow.java
@@ -1,12 +1,21 @@
 package ex1;
 
 import java.awt.EventQueue;
+import java.awt.Rectangle;
 
 import javax.swing.JFrame;
+import java.awt.Window.Type;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class SaludadorWindow {
 
-	private JFrame frame;
+	private JFrame Saludador;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -16,7 +25,7 @@ public class SaludadorWindow {
 			public void run() {
 				try {
 					SaludadorWindow window = new SaludadorWindow();
-					window.frame.setVisible(true);
+					window.Saludador.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +44,27 @@ public class SaludadorWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Saludador = new JFrame();
+		Saludador.setTitle("Saludador");
+		Saludador.setBounds(new Rectangle(0, 0, 450, 300));
+		Saludador.setLocationRelativeTo(null);
+		Saludador.getContentPane().setLayout(null);
+		
+		JLabel label = new JLabel("Escribe un nombre para saludar");
+		label.setBounds(114, 12, 244, 54);
+		Saludador.getContentPane().add(label);
+		
+		JButton boton = new JButton("¡Saludar!");
+		boton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		boton.setBounds(160, 199, 117, 25);
+		Saludador.getContentPane().add(boton);
+		
+		JTextField texto = new JTextField();
+		texto.setBounds(83, 132, 293, 19);
+		Saludador.getContentPane().add(texto);
+		textField.setColumns(10);
 	}
-
 }
