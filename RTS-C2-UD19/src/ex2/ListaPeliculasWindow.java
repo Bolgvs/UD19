50cdb31c15561325636444cb2499f3076536b92e
Bolgvs <106079820+Bolgvs@users.noreply.github.com>
2022-07-29 10:17:38 +0200

    commit ventana ex2

--- RTS-C2-UD19/src/ex2/ListaPeliculasWindow.java
+++ RTS-C2-UD19/src/ex2/ListaPeliculasWindow.java
@@ -1,5 +1,72 @@
 package ex2;
 
-public class ListaPeliculasWindow {
 
-}
+	import java.awt.EventQueue;
+
+	import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+
+	public class ListaPeliculasWindow {
+
+	    private JFrame frmPeliculas;
+	    private JTextField textField;
+
+	    /*
+	     * Launch the application.
+	     */
+	    public static void main(String[] args) {
+	        EventQueue.invokeLater(new Runnable() {
+	            public void run() {
+	                try {
+	                	ListaPeliculasWindow window = new ListaPeliculasWindow();
+	                    window.frmPeliculas.setVisible(true);
+	                } catch (Exception e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        });
+	    }
+
+	    /*
+	     * Create the application.
+	     */
+	    public ListaPeliculasWindow() {
+	        initialize();
+	    }
+
+	    /**
+	     * Initialize the contents of the frame.
+	     */
+	    private void initialize() {
+	        frmPeliculas = new JFrame();
+	        frmPeliculas.setTitle("Peliculas");
+	        frmPeliculas.setBounds(0, 0, 450, 300);
+	        frmPeliculas.setLocationRelativeTo(null);
+	        frmPeliculas.getContentPane().setLayout(null);
+	        
+	        JLabel lblNewLabel = new JLabel("Escribe el t\u00EDtulo de una pel\u00EDcula");
+	        lblNewLabel.setBounds(31, 74, 153, 33);
+	        frmPeliculas.getContentPane().add(lblNewLabel);
+	        
+	        JLabel lblNewLabel_1 = new JLabel("Pel\u00EDculas");
+	        lblNewLabel_1.setBounds(318, 83, 46, 14);
+	        frmPeliculas.getContentPane().add(lblNewLabel_1);
+	        
+	        textField = new JTextField();
+	        textField.setBounds(31, 118, 153, 20);
+	        frmPeliculas.getContentPane().add(textField);
+	        textField.setColumns(10);
+	        
+	        JButton btnNewButton = new JButton("A\u00F1adir");
+	        btnNewButton.setBounds(58, 164, 89, 23);
+	        frmPeliculas.getContentPane().add(btnNewButton);
+	        
+	        JComboBox comboBox = new JComboBox();
+	        comboBox.setBounds(289, 117, 96, 22);
+	        frmPeliculas.getContentPane().add(comboBox);
+	        frmPeliculas.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    }
+	}
